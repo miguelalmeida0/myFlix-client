04bfd7e8fee22e915ddea6f1ef0005bbe72a954a
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2022-01-17 13:51:59 +0100

    problems

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { MovieCard } from '../movie-card/movie-card';
+
 
 export class MainView extends React.Component {
 
@@ -10,22 +12,35 @@ export class MainView extends React.Component {
         { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...' },
         { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...' },
         { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...' }
-      ]
-    }
+      ],
+      selectedMovie: null
+    };
+
+  }
+
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie
+    });
   }
 
   render() {
-    const movies = this.state.movies;
-    if (movies.length === 0) {
-      return <div className="main-view">The list is empty!</div>;
-    } else {
-      return (
-        <div className="main-view">
-          {movies.map((movie) => {
-            return <div>{movie.Title}</div>;
-          })}
-        </div>
-      );
-    }
+    const { movies, selectedMovie } = this.state;
+
+    if (selectedMovie) return <MovieView movie={selectedMovie} />;
+
+    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
+    return (
+      <div className="main-view">
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+          ))
+        }
+      </div>
+    );
   }
+
 }
