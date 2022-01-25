de1bc5175038313a1fc66ade0b7bcee311f7429e
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2022-01-25 14:09:25 +0100

    Parcel error on the terminal 3.4

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
@@ -9,13 +10,21 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Soul', Description: 'Joe is a middle-school band teacher whose life hasn\'t quite gone the way he expected. His true passion is jazz and he\'s good. But when he travels to another realm to help someone find their passion, he soon discovers what it means to have soul.', ImagePath: 'https://lumiere-a.akamaihd.net/v1/images/hb_soul_header_mobile_18983_7bfc5464.jpeg?region=0,0,640,560', Genre: 'Animation', Director: 'Pete Docter' },
-        { _id: 2, Title: 'Don\'t look up', Description: 'Two low-level astronomers must go on a giant media tour to warn mankind of an approaching comet that will destroy planet Earth.', ImagePath: 'https://lh3.googleusercontent.com/-nXzyRAt-WXA/Yccsv1DXx6I/AAAAAAAAgA0/MA25GSrmkEo8Zn0DLg9XVNKERCrbB-ABACNcBGAsYHQ/s1600/1640443071740243-0.png', Genre: 'Comedy', Director: 'Adam McKay' },
-        { _id: 3, Title: 'Harry Potter and the Sorcerer\'s Stone', Description: 'An orphaned boy enrolls in a school of wizardry, where he learns the truth about himself, his family and the terrible evil that haunts the magical world.', ImagePath: 'https://images-na.ssl-images-amazon.com/images/I/51rV-Hh+mKL.jpg', Genre: 'Fantasy', Director: 'Chris Columbus' }
-      ],
+      movies: [],
       selectedMovie: null
     };
+  }
+
+  componentDidMount() {
+    axios.get('https://driveindb.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
 
