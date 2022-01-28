18c7a72ca00c8b8405b84db1025a61383e196132
miguelalmeida0 <94702822+miguelalmeida0@users.noreply.github.com>
2022-01-28 12:31:49 +0100

    added propTypes to movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -19,3 +19,12 @@ export class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
